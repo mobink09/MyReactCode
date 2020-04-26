c9a25996546366508b45f914fd8c82429e6394f5
Mobin Arshad <mobink09@gmail.com>
2020-04-26 13:33:32 +0530

    In this version component of table made

--- table/src/App.js
+++ table/src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+
+function Button(){
+  return <button>Buy Now</button>
+}
+
+function Td(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr>
+      <td>{props.yearValue}</td>
+      <td>{props.modelValue}</td>
+      <td>{props.priceValue}</td>
+      <td><Button /></td>
+    </tr>
+  );
+}
+
+function Th(props) {
+  return (
+    <tr>
+      <th>{props.year}</th>
+      <th>{props.model}</th>
+      <th>{props.price}</th>
+      <th>{props.buy}</th>
+    </tr>
+  );
+}
+
+function Table(props) {
+  return (
+    <table>
+      <Th year="Years" model="Model" price="Price" buy="Buy" />
+      <Td yearValue = {2019} modelValue = {'M'} priceValue = {"$70000"}/>
+      <Td yearValue = {2018} modelValue = {'O'} priceValue = {"$60000"}/>
+      <Td yearValue = {2020} modelValue = {'B'} priceValue = {"$75000"}/>
+      <Td yearValue = {2018} modelValue = {'I'} priceValue = {"$70000"}/>
+      <Td yearValue = {2016} modelValue = {'N'} priceValue = {"$100000"}/>
+      <Td yearValue = {2019} modelValue = {'A'} priceValue = {"$70000"}/>
+      <Td yearValue = {2019} modelValue = {'R'} priceValue = {"$20000"}/>
+      <Td yearValue = {2019} modelValue = {'S'} priceValue = {"$30000"}/>
+    </table>
+  );
+}
+
+function App(props) {
+  return (
+    <div className= "container">
+      <h1>{props.heading}</h1>
+      <Table />
     </div>
   );
 }
--- table/src/index.js
+++ table/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App heading = "Cars"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
